089b147ca7423dfed09a772c434bc4c50c043434
oluwasube <enibeke20@gmail.com>
2023-02-04 00:10:30 +0100

    :bug: fix bug

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,8 @@ import os
 import mysql.connector
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
