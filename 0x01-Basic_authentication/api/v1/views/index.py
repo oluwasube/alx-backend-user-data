bed9a396145fa5ad866c081ac9f01cdeef8f20a8
oluwasube <enibeke20@gmail.com>
2023-02-07 21:45:49 +0100

    task 2 completed

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized():
     """ Unauthorized endpoint
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ Forbidden endpoint
+    """
+    abort(403)
